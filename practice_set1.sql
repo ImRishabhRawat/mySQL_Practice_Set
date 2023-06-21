199cd5407fdc0f05a0749228362fe45e1454ee38
ImRishabhRawat <rishabhrawat757@gmail.com>
2023-06-21 11:27:42 +0530

    changes in practice set

--- practice_set1.sql
+++ practice_set1.sql
@@ -9,7 +9,32 @@ INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (2, 'V
 INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (3, 'Mayank Pathak', 'mayank@gmail.com');
 INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (4, 'Harish Solanki', 'harish@outlook.com', 1);
 INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (5, 'Manoj Saru', 'manoj@yahoo.com', 1);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (6, 'Suraj Pandey', 'suraj@gmail.com', 3);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (7, 'Arjun barthwal', 'arjun@gmail.com');
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (8, 'Ayush Rawat', 'ayush@outlook.com'7);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (9, 'Abhishek Butola', 'abhishek@gmail.com', 7);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (10, 'Akhilesh Pandey', 'akhilesh@yahoo.com', 3);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (11, 'Rahul Dobariyal', 'rahul@outlook.com');
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (12, 'Praveen Kumar', 'praveen@yahoo.com');
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (13, 'Fukra Insaan', 'fukra@gmail.com', 11);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (14, 'Rajat Shukla', 'rajat@gmail.com', 3);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (15, 'Soni Khan', 'soni@yahoo.com', 7);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (16, 'Ananya Pandey', 'ananya@outlook.com', 12);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (17, 'Sid Sem', 'sidsem@yahoo.com', 11);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (18, 'Kareena Kapoor', 'kareena@gmail.com', 12);
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`) VALUES (19, 'Sahid Kapoor', 'sahid@outlook.com');
+INSERT INTO `College`.`college_individual` (`id`, `Name`, `Email`, `Mentor_id`) VALUES (20, 'Logan Paul', 'logan@yahoo.com', 19);
 
-Select * from college_individual order by Name;
+SELECT * from college_individual WHERE
+Mentor_id IS NOT NULL 
+ORDER BY `Name` ASC
 
-Select * from college_individual where Email like '%gmail.com';
+
+SELECT e.name AS mentee , m.name AS mentor
+from college_individual e
+JOIN college_individual m ON e.Mentor_id = m.id
+
+SELECT e.name AS mentee, m.name AS mentor
+from college_individual e
+JOIN college_individual m ON e.Mentor_id = m.id
+WHERE m.Email LIKE '%gmail.com'
